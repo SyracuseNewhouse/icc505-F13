d83b514e998a9f9fa6e79bae8bee11edb3da7fed
tcharles411 <tcharles@syr.edu>
2013-11-18 20:13:50 -0500

    Added cards/notes, joint investigation page

---
+++ groups/3/js/custom.js
@@ -0,0 +1,20 @@
+//$(function() {
+//	$("#joint_investigations").click(function(){
+		// This gets called when you click on the joint investigations card
+//		window.location = "";
+//	});
+//})
+
+jQuery(document).ready(function() {
+
+	//jQuery(".card").hover(function() {
+	//	jQuery(this).stop().animate({
+
+	//	})
+
+//});
+
+
+
+
+}); //ready method end
