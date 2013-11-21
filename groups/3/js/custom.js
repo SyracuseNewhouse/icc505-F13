b75aab637feda65f66c359ecbe6eca6cf452f316
tcharles411 <tcharles@syr.edu>
2013-11-21 14:22:07 -0500

    Finished Joint Investigation page functionality

--- groups/3/js/custom.js
+++ groups/3/js/custom.js
@@ -1,19 +1,44 @@
-//$(function() {
-//	$("#joint_investigations").click(function(){
-		// This gets called when you click on the joint investigations card
-//		window.location = "";
-//	});
-//})
-
 jQuery(document).ready(function() {
 
-	//jQuery(".card").hover(function() {
-	//	jQuery(this).stop().animate({
+	jQuery("#joint_investigations").click();
 
-	//	})
+	jQuery("#back").click();
 
-//});
+	jQuery("#map").hover(function() {
+		jQuery("#map_pop_up").css("display", "block");
+	}, function() {
+		jQuery("#map_pop_up").css("display", "none");
+	}); //map pop-up end
 
+	jQuery("#quote").hover(function(){
+		jQuery("#quote_pop_up").css("display", "block");
+	}, function() {
+		jQuery("#quote_pop_up").css("display", "none");
+	});
+
+	jQuery("#benefits").hover(function(){
+		jQuery("#benefits_pop_up").css("display", "block");
+	}, function() {
+		jQuery("#benefits_pop_up").css("display", "none");
+	});
+
+	jQuery("#sharing").hover(function (){
+		jQuery("#sharing_pop_up").css("display", "block");
+	}, function() {
+		jQuery("#sharing_pop_up").css("display", "none");
+	});
+
+	jQuery("#changes").hover(function(){
+		jQuery("#changes_pop_up").css("display", "block");
+	}, function(){
+		jQuery("#changes_pop_up").css("display", "none");
+	});
+
+	jQuery("#jttf").hover(function(){
+		jQuery("#jttf_pop_up").css("display", "block");
+	}, function(){
+		jQuery("#jttf_pop_up").css("display", "none");
+	});
 
 
 
